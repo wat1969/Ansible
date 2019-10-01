e0821093e5dfbfb00d65ab10400b15d570413367
wat1969 <s.tharawat@icloud.com>
2019-10-01 10:07:23 +0000

    First commit

---
+++ hello-world.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+func Handler(w http.ResponseWriter, r *http.Request) {
+	log.Print("Hello world received a request.")
+	target := os.Getenv("TARGET")
+	if target == "" {
+		target = "World"
+	}
+	fmt.Fprintf(w, "Hello %s!", target)
+}
+
+func main() {
+	log.Print("Hello world sample started.")
+
+	http.HandleFunc("/", Handler)
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
+}
