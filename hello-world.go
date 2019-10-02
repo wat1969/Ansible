9b6e7790579d1555220e9beec7ab739790b58ea2
wat1969 <s.tharawat@icloud.com>
2019-10-02 05:23:20 +0000

    add docker file

--- hello-world.go
+++ hello-world.go
@@ -11,7 +11,7 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 	log.Print("Hello world received a request.")
 	target := os.Getenv("TARGET")
 	if target == "" {
-		target = "World world world"
+		target = "Ansible"
 	}
 	fmt.Fprintf(w, "Hello %s!", target)
 }
